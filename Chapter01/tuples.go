8969a458448c19992a74878ee5b41be6dc116229
Christian Requena <442530+crekev@users.noreply.github.com>
2024-10-24 13:16:18 +0200

    Playing around with concurrency

--- Chapter01/tuples.go
+++ Chapter01/tuples.go
@@ -1,50 +1,58 @@
-//main package has examples shown
+// main package has examples shown
 // in Go Data Structures and algorithms book
 package main
 
 // importing fmt package
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-//gets the powerseries of integer a and returns tuple of square of a
+type tuple struct {
+	squr int
+	cube int
+}
+
+// gets the powerseries of integer a and returns tuple of square of a
 // and cube of a
+func powerSeriesConcurrent(a int, wg *sync.WaitGroup, c chan tuple) {
+	data := new(tuple)
+	data.cube, data.squr = a*a, a*a*a
+	c <- *data
+	time.Sleep(20 * time.Microsecond)
+
+	wg.Done()
+}
+
 func powerSeries(a int) (int, int) {
-
+	time.Sleep(20 * time.Microsecond)
 	return a * a, a * a * a
-
 }
 
-func powerSeriesN(a int) (square int, cube int) {
-
-	square = a * a
-
-	cube = square * a
-
-	return
-
-}
-
-func powerSeriesE(a int) (int, int, error) {
-
-	var square int = a * a
-
-	var cube int = square * a
-
-	return square, cube, nil
-
-}
-
-// main method
 func main() {
+	wg := new(sync.WaitGroup)
 
-	var square int
-	var cube int
-	square, cube = powerSeries(3)
+	// depending on the number of loops we need to increase the buffer
+	ch := make(chan tuple, 1024*1024)
+	loops := 20 // how many elements in the series to calculate
 
-	fmt.Println("Square ", square, "Cube", cube)
+	start_time := time.Now()
+	fmt.Println("Calculating concurrently ...")
+	for i := 3; i < loops; i++ {
+		go powerSeriesConcurrent(i, wg, ch)
+		wg.Add(1)
+	}
+	wg.Wait()
+	close(ch)
 
-	fmt.Println(powerSeriesN(4))
+	fmt.Println(time.Since(start_time), " passed")
 
-	fmt.Println(powerSeriesE(5))
+	start_time = time.Now()
+	fmt.Println("Calculating sequentially ...")
+	for i := 3; i < loops; i++ {
+		powerSeries(i)
+	}
+	fmt.Println(time.Since(start_time), " passed")
+
 }
